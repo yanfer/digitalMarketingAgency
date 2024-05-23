3a7d1ccaf08387c740448156bc3354da634e7f90
yanfer <60121284+yanfer@users.noreply.github.com>
2024-05-23 05:33:35 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -68,17 +68,17 @@ export default function Home() {
       <Spotlight className="hidden md:flex md:-top-80 left-80  " fill="white" />
       <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-20 px-2">
         <div className="text-4xl pb-5 md:text-7xl px-6 text-center  bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to bg-neutral-400 bg-opacity-50">
-          Create, grow, and <br /> scale your business
+          Crea, Crece y <br /> haz escalar tu negocio
         </div>
         <p className="mt-4 text-lg font-normal  text-neutral-300 max-w-lg text-center mx-auto px-4">
-          Custom tailored solutions for your business. We are a team of creatives who are excited to help you grow your business.
+          Soluciones personalizadas a medida para tu negocio. Somos un equipo de creativos entusiasmados de ayudarle a hacer crecer tu negocio.
         </p>
 
         <Link
           href={"/book"}
           className="cursor-pointer flex items-center justify-center border rounded-full w-48 p-2  mx-auto my-6 text-white "
         >
-          Book a call
+          Agendar cita
         </Link>
 
         <div className="w-full pt-20">
