8ab4babf055dda8e361d12aadbed6c5ec6ec7a59
yanfer <60121284+yanfer@users.noreply.github.com>
2024-05-23 06:05:38 -0600

    Update 3d-card-snippet.tsx

--- app/snippets/3d-card-snippet.tsx
+++ app/snippets/3d-card-snippet.tsx
@@ -12,14 +12,14 @@ export function ThreeDCardExample() {
           translateZ="50"
           className="text-xl font-bold text-neutral-600 dark:text-white"
         >
-          Make things float in air
+          Haz que las cosas floten en el aire.
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
           className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
         >
-          Hover over this card to unleash the power of CSS perspective
+          Pasa el cursor sobre esta tarjeta para liberar el poder de la perspectiva CSS
         </CardItem>
         <CardItem translateZ="100" className="w-full mt-4">
           <Image
@@ -36,14 +36,14 @@ export function ThreeDCardExample() {
             as="button"
             className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
           >
-            Try now →
+            Probar ahora →
           </CardItem>
           <CardItem
             translateZ={20}
             as="button"
             className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
           >
-            Sign up
+            Registrate
           </CardItem>
         </div>
       </CardBody>
