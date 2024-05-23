d2d1ba48e2eefeff64f71215d74239ca56d2e1fa
yanfer <60121284+yanfer@users.noreply.github.com>
2024-05-23 05:37:32 -0600

    Update graphic-design.tsx

--- app/graphic-design.tsx
+++ app/graphic-design.tsx
@@ -8,12 +8,11 @@ const GraphicDesign = () => {
     return ( <div>
        <div className=" p-4   mx-auto relative z-10  w-full pt-20 md:pt-32">
       <div className="text-4xl md:pb-8 md:text-7xl text-center bg-clip-text text-transparent bg-gradient-to-b from-purple-500 to-sky-200 bg-opacity-50">
-        Graphic Design <br />
+        Diseño Gráfico<br />
       </div>
 
       <p className="mt-4 text-lg font-normal  text-neutral-300 max-w-lg text-center mx-auto">
-        We create stunning visuals for your brand. From logos to social media
-        posts, we&apos;ve got you covered.
+        Creamos imágenes impresionantes para tu marca. Desde logotipos hasta publicaciones en redes sociales, lo tenemos cubierto.
       </p>
       <div className="items-center md:flex justify-center md:mx-auto md:space-x-10">
       <motion.div
