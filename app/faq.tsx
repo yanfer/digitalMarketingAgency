8bb09cd5a7b10747af4e6fc9b4811c1308f9fece
yanfer <60121284+yanfer@users.noreply.github.com>
2024-05-23 05:40:21 -0600

    Update faq.tsx

--- app/faq.tsx
+++ app/faq.tsx
@@ -15,40 +15,40 @@ const FAQS = () => {
         ">
           <div className=" p-10 md:p-4 md:px-20">
             <div className="text-3xl md:text-7xl font-bold text-black">
-              Have questions ?
+              Tiene preguntas ?
             </div>
             <div className="  font-semibold text-3xl md:text-6xl text-gradient bg-gradient-to-r from-emerald-600 to-blue-300 bg-clip-text text-transparent">
-              Get answers.
+              Obten respuestas.
             </div>
             <Accordion type="single" collapsible>
     <AccordionItem value="item-1">
-      <AccordionTrigger>What is Bird?</AccordionTrigger>
+      <AccordionTrigger>Que es Y-N ?</AccordionTrigger>
       <AccordionContent>
-        Bird is a full fledge marketing agency that specializes in branding, web design, and digital marketing.
+        Y-N es una agencia de marketing completa que se especializa en branding, diseño web y marketing digital.
       </AccordionContent>
     </AccordionItem>
     <AccordionItem value="item-2">
-      <AccordionTrigger>How to start ?</AccordionTrigger>
+      <AccordionTrigger>Como comenzar ?</AccordionTrigger>
       <AccordionContent>
-        You can start by contacting us. We will get back to you within 24 hours.
+        Puedes empezar poniéndote en contacto con nosotros. Nos comunicaremos contigo dentro de las 24 horas.
       </AccordionContent>
     </AccordionItem>
     <AccordionItem value="item-3">
       <AccordionTrigger>
-    Pricing ?
+    Precios ?
       </AccordionTrigger>
       <AccordionContent>
-        We offer custom tailored solutions for your business. Contact us to get a quote.
+        Ofrecemos soluciones personalizadas a medida para su negocio. Contáctanos para obtener una cotización.
         
       </AccordionContent>
     </AccordionItem>
   
     <AccordionItem value="item-4">
       <AccordionTrigger>
-    Support ?
+    Soporte ?
         </AccordionTrigger>
       <AccordionContent>
-        We offer Monday-Friday support for all our clients.
+        Ofrecemos soporte de lunes a viernes para todos nuestros clientes.
       </AccordionContent>
     </AccordionItem>
   </Accordion>
